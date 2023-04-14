4a1c71b1e014b36e93185f9139e6be724d90e475
rohit jain <rohit1987j@gmail.com>
2023-04-14 22:03:12 +0200

    bug-1 fixed.

--- newsfeedgenerator/src/main/java/socketclient/RequestSender.java
+++ newsfeedgenerator/src/main/java/socketclient/RequestSender.java
@@ -1,42 +1,23 @@
 package socketclient;
 
-import data.NewsFeed;
-
 import service.NewsFeedGenerator;
-
-import java.io.IOException;
-import java.io.ObjectOutputStream;
-
-import java.net.Socket;
-
-import java.util.ArrayList;
-import java.util.List;
 
 public class RequestSender implements Runnable {
     private final int frequency;
+    private final SocketClient socketClient;
 
-    public RequestSender(int frequency) {
+    public RequestSender(int frequency, SocketClient socketClient) {
         this.frequency = frequency;
+        this.socketClient = socketClient;
     }
 
     @Override
     public void run() {
 
-        try (
-                Socket echoSocket = new Socket("127.0.0.1", 8088);
-                ObjectOutputStream outputStream = new ObjectOutputStream(echoSocket.getOutputStream());
-        ) {
-
-
             for (int counter = 0; counter < frequency; counter++) {
 
-                outputStream.writeObject(new NewsFeedGenerator().generate());
+                socketClient.writeNewsFeed(new NewsFeedGenerator().generate());
 
             }
-            System.out.println("newsfeed send to server");
-
-        } catch (IOException e) {
-            System.err.println("Error while writing to server" +  e);
-        }
     }
 }
--- newsfeedgenerator/src/main/java/socketclient/SocketClient.java
+++ newsfeedgenerator/src/main/java/socketclient/SocketClient.java
@@ -1,16 +1,98 @@
 package socketclient;
 
+import data.NewsFeed;
+
+import java.io.IOException;
+import java.io.ObjectOutputStream;
+
+import java.net.Socket;
+
 import java.util.concurrent.Executors;
 import java.util.concurrent.TimeUnit;
 
 public class SocketClient {
+    private Socket echoSocket = null;
+    private ObjectOutputStream outputStream = null;
+
     public void connect(int frequency)  {
 
+        Runtime.getRuntime().addShutdownHook(new Thread(() -> {
+            closeOutputStream();
+            closeSocket();
+        }));
+
+        connectToServer();
+
+        initializeOutputStream();
+
         Executors.newSingleThreadScheduledExecutor().scheduleAtFixedRate(
-                new RequestSender(frequency),
+                new RequestSender(frequency, this),
                 1,
                 1,
                 TimeUnit.SECONDS
         );
+    }
+
+    private void initializeOutputStream() {
+        while (outputStream == null) {
+            try {
+                outputStream = new ObjectOutputStream(echoSocket.getOutputStream());
+            } catch (IOException e) {
+                System.err.println("Error while connecting to server: 127.0.0.1:8088" +  e);
+            }
+        }
+    }
+
+    private void connectToServer() {
+        while (echoSocket == null || echoSocket.isClosed()) {
+            try {
+                echoSocket = new Socket("127.0.0.1", 8088);
+            } catch (IOException e) {
+                System.err.println("Error while connecting to server: 127.0.0.1:8088" +  e);
+            }
+        }
+    }
+
+    private void closeOutputStream() {
+        System.err.println("closing outputstream");
+        if (outputStream != null) {
+            try {
+                outputStream.close();
+                outputStream = null;
+            } catch (IOException failed) {
+                //failed
+            }
+        }
+    }
+
+    private void closeSocket() {
+        System.err.println("closing socket");
+        if (echoSocket != null) {
+            try {
+                echoSocket.close();
+                echoSocket = null;
+                outputStream = null;
+            } catch (IOException failed) {
+                //failed
+            }
+        }
+    }
+
+    public void writeNewsFeed(NewsFeed newsFeed) {
+        try {
+
+            outputStream.writeObject(newsFeed);
+            System.out.println("newsfeed send to server");
+
+        } catch (IOException e) {
+            System.err.println("failed to send data to server." + e);
+            reconnect();
+        }
+    }
+
+    private void reconnect() {
+        closeSocket();
+        connectToServer();
+        initializeOutputStream();
     }
 }
