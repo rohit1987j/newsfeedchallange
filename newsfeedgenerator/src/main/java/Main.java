f5654adad2f4171b4b1f283186b59e7a6d38aeb2
rohit jain <rohit1987j@gmail.com>
2023-04-14 22:02:36 +0200

    bug-3 fixed.

--- newsfeedgenerator/src/main/java/Main.java
+++ newsfeedgenerator/src/main/java/Main.java
@@ -2,19 +2,29 @@ import socketclient.SocketClient;
 
 public class Main {
     public static void main(String args[]) {
-        if (args.length != 1) {
+
+        int frequency = getFrequency(args);
+
+        SocketClient socketClient = new SocketClient();
+        socketClient.connect(frequency);
+    }
+
+    private static Integer getFrequency(String[] args) {
+        String frequencyString = System.getProperty("frequency");
+
+        if (frequencyString == null) {
             System.err.println("Please enter frequency of items sent per second <frequency>");
             System.exit(1);
         }
+
         Integer frequency = null;
         try {
-            frequency = Integer.parseInt(args[0]);
+            frequency = Integer.parseInt(frequencyString);
         } catch (NumberFormatException e) {
             System.err.println(String.format("frequency has to be integer, invalid frequency: %s", args[0]));
             System.exit(1);
         }
 
-        SocketClient socketClient = new SocketClient();
-        socketClient.connect(frequency);
+        return frequency;
     }
 }
